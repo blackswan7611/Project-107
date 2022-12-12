e804ff73b7295c6f58f44cd7d11d0e8ed9615ff9
blackswan7611 <94906126+blackswan7611@users.noreply.github.com>
2022-12-12 21:04:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifer = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/iexwvr9Ha/model.json", modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
